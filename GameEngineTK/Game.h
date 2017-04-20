feb2445f47b5ed51864dedb72b4bc3f7dbda050c
yuya okada <crown.got9610@gmail.com>
2017-04-20 10:42:12 +0900

    カメラ実装、ポリゴン描画

--- GameEngineTK/Game.h
+++ GameEngineTK/Game.h
@@ -6,11 +6,21 @@
 
 #include "StepTimer.h"
 
+#include <PrimitiveBatch.h>
+#include <VertexTypes.h>
+#include <Effects.h>
+#include <CommonStates.h>
+#include <SimpleMath.h>
+
+#include "DebugCamera.h"
+
+
 
 // A basic game implementation that creates a D3D11 device and
 // provides a game loop.
 class Game
 {
+
 public:
 
     Game();
@@ -62,4 +72,16 @@ private:
 
     // Rendering loop timer.
     DX::StepTimer                                   m_timer;
+
+	//std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>> m_batch;
+	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionNormal>> m_batch;
+	std::unique_ptr<DirectX::BasicEffect> m_effect;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
+
+	DirectX::SimpleMath::Matrix m_world;
+	DirectX::SimpleMath::Matrix m_view;
+	DirectX::SimpleMath::Matrix m_proj;
+	
+	//デバックカメラ
+	std::unique_ptr<DebugCamera> m_debugCamera;
 };
